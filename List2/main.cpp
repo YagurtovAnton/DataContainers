7c4a1171683f997e439fc95f3e0cfe2dde39ca73
YagurtovAnton <Yagur_oo18@top-academy.ru>
2024-08-28 20:56:14 +0300

    list TODO

--- List2/main.cpp
+++ List2/main.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
 using namespace std;
 
+#define tab "\t"
+#define delimiter "\n-------------------------------------------------\n"
+
+class List
+{
+	class Elemert
+	{
+		int Data;
+		Elemert* pNext;
+		Elemert* pPrev;
+	public:
+		Elemert(int Data, Elemert* pNext = nullptr, Elemert* pPrev = nullptr)
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Elemert()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head, * Tail;
+	size_t size;
+};
+	
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
 }
